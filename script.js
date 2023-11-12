ffc8f9732f8260d3a447bf46b18ff3eef73663a1
Tumanish <59100693+Tumanish@users.noreply.github.com>
2023-11-12 12:47:14 +0300

    stars

--- script.js
+++ script.js
@@ -57,7 +57,19 @@ function videoSelect(element) {
 
 
 
+function starClick(element) {
+	let empty = document.getElementById("section-main-video__star--img1");
+	let full = document.getElementById("section-main-video__star--img2");
 
+	if (empty.classList.contains("main-video__star--hide")) {
+		empty.classList.remove("main-video__star--hide");
+	full.classList.add("main-video__star--hide");
+	}else{
+		empty.classList.add("main-video__star--hide");
+		full.classList.remove("main-video__star--hide");
+	}
+
+}
 
 
 
