655a2e8b16ba318343d2cdb26883a33b026e2887
Tumanish <59100693+Tumanish@users.noreply.github.com>
2023-11-12 13:12:27 +0300

    video select

--- script.js
+++ script.js
@@ -28,23 +28,34 @@ function fotoSelect(element) {
 }
 
 
+let count = 1;
 function videoSelect(element) {
 
 	let videoArray = [
-		"https://www.youtube.com/embed/hJnGmgrzs44?si=NixvF1eBouhqoiZh;controls=0",
-		"https://www.youtube.com/embed/FTSQfkQLuro?si=Jx9V7Tql5WCHC9tk&amp;controls=0",
+		"https://www.youtube.com/embed/8dmlK2yoLuk?si=butTfBtmXzHqr_7G;controls=0",
+		"https://www.youtube.com/embed/WhNcqdZCgPE?si=IdC52uYUwatJlRbA;controls=0",
 
 	]
 
 
 	let test = document.getElementById("selected-video");
-	console.log(test.src);
-	test.src = videoArray[0];
+
+	switch (count) {
+		case 1:
+			test.src = videoArray[0];
+			count = 0;
+			break;
+		case 0:
+			test.src = videoArray[1];
+			count = 1;
+			break;
+	}
+
+	// test.src = videoArray[1];
 
 
 
-
-	// style
+	// style убрать рамку
 	let swp = document.getElementById("swiperVideo").querySelectorAll(".swiper-slide--background");
 	console.log(swp);
 	for (let i = 0; i < swp.length; i++) {
@@ -63,14 +74,13 @@ function starClick(element) {
 
 	if (empty.classList.contains("main-video__star--hide")) {
 		empty.classList.remove("main-video__star--hide");
-	full.classList.add("main-video__star--hide");
-	}else{
+		full.classList.add("main-video__star--hide");
+	} else {
 		empty.classList.add("main-video__star--hide");
 		full.classList.remove("main-video__star--hide");
 	}
 
 }
-
 
 
 
